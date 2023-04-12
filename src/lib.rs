c527ca88fd3ff2b881139998540ee78c2c546ad5
gjf2a <ferrer@hendrix.edu>
2023-04-12 07:40:59 -0500

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,162 @@
+#![no_std]
+#![feature(prelude_2024)]
+
+// use file_system_solution::{FileSystem, FileSystemResult};
+use pc_keyboard::{DecodedKey, KeyCode};
+use pluggable_interrupt_os::vga_buffer::{BUFFER_WIDTH, BUFFER_HEIGHT, plot, ColorCode, Color, plot_str, is_drawable, plot_num};
+use ramdisk::RamDisk;
+use simple_interp::{Interpreter, InterpreterOutput, i64_into_buffer};
+// use gc_heap::CopyingHeap;
+
+// Get rid of some spurious VSCode errors
+use core::option::Option;
+use core::option::Option::None;
+use core::prelude::rust_2024::derive;
+use core::clone::Clone;
+use core::cmp::{PartialEq,Eq};
+use core::marker::Copy;
+
+const FIRST_BORDER_ROW: usize = 1;
+const LAST_BORDER_ROW: usize = BUFFER_HEIGHT - 1;
+const TASK_MANAGER_WIDTH: usize = 10;
+const TASK_MANAGER_BYTES: usize = BUFFER_HEIGHT * TASK_MANAGER_WIDTH;
+const WINDOWS_WIDTH: usize = BUFFER_WIDTH - TASK_MANAGER_WIDTH;
+const WINDOW_WIDTH: usize = (WINDOWS_WIDTH - 3) / 2;
+const WINDOW_HEIGHT: usize = (LAST_BORDER_ROW - FIRST_BORDER_ROW - 2) / 2;
+const MID_WIDTH: usize = WINDOWS_WIDTH / 2;
+const MID_HEIGHT: usize = BUFFER_HEIGHT / 2;
+const NUM_WINDOWS: usize = 4;
+
+const FILENAME_PROMPT: &str = "F5 - Filename: ";
+
+const MAX_OPEN: usize = 16;
+const BLOCK_SIZE: usize = 256;
+const NUM_BLOCKS: usize = 255;
+const MAX_FILE_BLOCKS: usize = 64;
+const MAX_FILE_BYTES: usize = MAX_FILE_BLOCKS * BLOCK_SIZE;
+const MAX_FILES_STORED: usize = 30;
+const MAX_FILENAME_BYTES: usize = 10;
+
+const MAX_TOKENS: usize = 500;
+const MAX_LITERAL_CHARS: usize = 30;
+const STACK_DEPTH: usize = 50;
+const MAX_LOCAL_VARS: usize = 20;
+const HEAP_SIZE: usize = 1024;
+const MAX_HEAP_BLOCKS: usize = HEAP_SIZE;
+
+// Data type for a file system object:
+// FileSystem<MAX_OPEN, BLOCK_SIZE, NUM_BLOCKS, MAX_FILE_BLOCKS, MAX_FILE_BYTES, MAX_FILES_STORED, MAX_FILENAME_BYTES>
+
+// Data type for an interpreter object:
+// Interpreter<MAX_TOKENS, MAX_LITERAL_CHARS, STACK_DEPTH, MAX_LOCAL_VARS, WINDOW_WIDTH, CopyingHeap<HEAP_SIZE, MAX_HEAP_BLOCKS>>
+
+
+pub struct Kernel {
+    // YOUR CODE HERE
+}
+
+const HELLO: &str = r#"print("Hello, world!")"#;
+
+const NUMS: &str = r#"print(1)
+print(257)"#;
+
+const ADD_ONE: &str = r#"x := input("Enter a number")
+x := (x + 1)
+print(x)"#;
+
+const COUNTDOWN: &str = r#"count := input("count")
+while (count > 0) {
+    count := (count - 1)
+}
+print("done")
+print(count)"#;
+
+const AVERAGE: &str = r#"sum := 0
+count := 0
+done := false
+while not done {
+    num := input("Enter a number:")
+    if num == "quit" {
+        done := true
+    } else {
+        sum := sum + num
+        count := count + 1
+    }
+}
+print(sum / count)"#;
+
+const PI: &str = r#"sum := 0
+i := 0
+neg := false
+terms := input("Num terms:")
+while i < terms {
+    term := 1 / ((2 * i) + 1)
+    if neg {
+        term := -term
+    }
+    sum := sum + term
+    neg := not neg
+    i := i + 1
+}
+print(4 * sum)"#;
+
+/*
+// Seed the disk with some programs.
+fn initial_files(disk: &mut FileSystem<MAX_OPEN, BLOCK_SIZE, NUM_BLOCKS, MAX_FILE_BLOCKS, MAX_FILE_BYTES, MAX_FILES_STORED, MAX_FILENAME_BYTES>) {
+    for (filename, contents) in [
+        ("hello", HELLO),
+        ("nums", NUMS),
+        ("add_one", ADD_ONE),
+        ("countdown", COUNTDOWN),
+        ("average", AVERAGE),
+        ("pi", PI),
+    ] {
+        let fd = disk.open_create(filename).unwrap();
+        disk.write(fd, contents.as_bytes()).unwrap();
+        disk.close(fd);
+    }
+}
+*/
+
+impl Kernel {
+    pub fn new() -> Self {
+        todo!("Create your kernel object");
+    }
+
+    pub fn key(&mut self, key: DecodedKey) {
+        match key {
+            DecodedKey::RawKey(code) => self.handle_raw(code),
+            DecodedKey::Unicode(c) => self.handle_unicode(c)
+        }
+        self.draw();
+    }
+
+    fn handle_raw(&mut self, key: KeyCode) {
+        todo!("handle unprintable keys");
+    }
+
+    fn handle_unicode(&mut self, key: char) {
+        todo!("handle printable keys");
+    }
+
+    pub fn draw(&mut self) {
+        todo!("Draw the kernel");
+    }
+
+    pub fn draw_proc_status(&mut self) {
+        todo!("Draw processor status");
+    }
+
+    pub fn run_one_instruction(&mut self) {
+        todo!("Run an instruction in a process");
+    }
+}
+
+fn text_color() -> ColorCode {
+    ColorCode::new(Color::White, Color::Black)
+}
+
+fn highlight_color() -> ColorCode {
+    ColorCode::new(Color::Black, Color::White)
+}
+
